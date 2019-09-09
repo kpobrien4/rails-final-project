e1d9aa2e28dc027458bc84348a3d4b1cd09c05b3
github username <github email address>
2019-09-09 08:15:11 -0400

    Added projects resource

---
+++ db/migrate/20190909121134_create_projects.rb
@@ -0,0 +1,14 @@
+class CreateProjects < ActiveRecord::Migration[5.2]
+  def change
+    create_table :projects do |t|
+      t.string :title
+      t.string :genre
+      t.text :info
+      t.string :contact
+      t.integer :user_id
+      t.integer :skillset_id
+
+      t.timestamps
+    end
+  end
+end
